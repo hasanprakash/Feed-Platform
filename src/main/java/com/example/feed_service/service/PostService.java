f2cd07d9c022c274a567d20e30d5e1429ff5fc39
Hasan Prakash <hasanchadaram888@gmail.com>
2026-07-26 23:24:29 +0530

    Establishing the Base Line

--- src/main/java/com/example/feed_service/service/PostService.java
+++ src/main/java/com/example/feed_service/service/PostService.java
@@ -1,14 +1,12 @@
 package com.example.feed_service.service;
 
 import com.example.feed_service.model.Post;
-import com.example.feed_service.repository.FollowerRepository;
 import com.example.feed_service.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.util.Collections;
 import java.util.List;
 
 @Service
